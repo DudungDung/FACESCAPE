0df8b757982571b9c34ebae086cf68b335cf1516
HoJin Desktop <ghwls328@gmail.com>
2020-04-16 02:04:50 +0900

    얼굴인식 동기화

--- face_detect.py
+++ face_detect.py
@@ -1,11 +1,11 @@
 import cv2
 
-def face_detect(image_file):
 
-    cascade_file = "C:\\Users\\sec\\Desktop\\haarcascade_frontalface_alt.xml"
+def face_detect(image):
+    cascade_file = "data/haarcascade_frontalface_alt.xml"
 
-    image = cv2.imread(image_file)
-    print(image)
+    # image = cv2.imread(image_file)
+    # print(image)
 
     image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -18,20 +18,24 @@ def face_detect(image_file):
         color = (0, 0, 255)
         for face in face_list:
             x, y, w, h = face
-            #print(x, y, w, h);
-            face_img = image[y:y+h, x:x+w];
-            #face_img = image[(x*7)//5:y+(h*4)//5, (x*7)//5:x+(w*4)//5];
-            face_img = cv2.resize(face_img, (w//20, h//20));
+            # print(x, y, w, h);
+            face_img = image[y:y + h, x:x + w];
+            # face_img = image[(x*7)//5:y+(h*4)//5, (x*7)//5:x+(w*4)//5];
+            face_img = cv2.resize(face_img, (w // 20, h // 20));
             face_img = cv2.resize(face_img, (w, h), interpolation=cv2.INTER_AREA);
-            image[y:y+h, x:x+h] = face_img
-           # cv2.rectangle(image, (x, y), (x+w, y+h), color, thickness = 8)
+            image[y:y + h, x:x + h] = face_img
+            # cv2.rectangle(image, (x, y), (x+w, y+h), color, thickness = 8)
 
-            adr = input("저장주소 :: ")
-            cv2.imwrite(adr, image)
+            # adr = input("저장주소 :: ")
+            # cv2.imwrite(adr, image)
+
+            print("find face")
+            return image
 
     else:
-        print("no face")
+        return image
 
-image_file = input("이미지 파일 :: ")
 
-face_detect(image_file)
+#image_file = input("이미지 파일 :: ")
+
+#face_detect(image_file)
