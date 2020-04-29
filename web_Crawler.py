0b464d0a430d0327a7a6602a697aa9ded378e8ad
MaShuu-Studio <ghwls328@naver.com>
2020-04-30 07:33:50 +0900

    이미지 크롤링 구글->네이버

--- web_Crawler.py
+++ web_Crawler.py
@@ -1,31 +1,99 @@
 from urllib.request import urlopen
-import requests as req
+import requests
 from bs4 import BeautifulSoup
 import os
 
+import time
+import shutil
+
 
 def Crawling_Image(name, maxAmount):
+
+    # 이미지 url https://search.naver.com/search.naver?where=image&query=검색이름
+    # 네이버는 기본적으로 50개를 불러오는 방식을 이용함
+    # 이 때 start 인자를 이용하면 시작지점을 정할 수 있어 50개단위로 여러번 작동시켜 원하는만큼 받아오도록 함
+
+    # 원래 구글을 이용하려 하였으나 구글을 통해 받은 이미지 해상도가 너무 좋지 않아 네이버로 변경
+    # 또한 네이버는 이미지 소스에 data-source가 있는 경우를 통해 제한할 경우 구글처럼 로고가 포함되는 경우가 없어 오류 조정에도 더 편함
+    # 한국 인물 특성상 네이버를 통해서 많은 이미지를 얻을 수 있기 때문에 네이버로 이용
+
+    naverUrl = "https://search.naver.com/search.naver?"
+
+    startNum = 0
+    currentImageAmount = 1
+
+    # 기본적으로 요청량만큼 받지만 이미지 다운로드에 실패하여 startNum이 너무 늘어날 경우 그냥 끝냄
+    while currentImageAmount <= maxAmount and startNum < maxAmount * 3:
+        params = {
+            "query": name,
+            "where": "image",
+            "start": startNum
+        }
+
+        htmlData = requests.get(naverUrl, params)
+        if htmlData.status_code == 200:
+            soup = BeautifulSoup(htmlData.text, 'html.parser')
+            imgs = soup.find_all('img', {'data-source': True})
+
+            dirName = "data/IMG/" + name + '/'
+
+            try:
+                # 이미 파일이 있을 경우 삭제하고 다시 다운로드
+                # 나중에 빠른 기계학습을 위해 파일 재다운 유무를 물어봄
+                if os.path.exists(dirName):
+                    try:
+                        shutil.rmtree(dirName)
+                        # 삭제 후 바로 생성하면 충돌로 인해 문제가 생기기 때문에 1초 쉬어줌
+                        time.sleep(1)
+                        print("Remove Directory: " + dirName)
+                    except OSError:
+                        print("Error: Remove directory: " + dirName)
+                os.makedirs(dirName)
+                print("Create Directory: " + dirName)
+            except OSError:
+                print("Error: Creating directory: " + dirName)
+
+            for i in enumerate(imgs):
+                try:
+                    img = urlopen(i[1].attrs['data-source']).read()
+                    filename = dirName + name + str(currentImageAmount) + '.jpg'
+                    with open(filename, 'wb') as f:
+                        f.write(img)
+                        print(i[1].attrs['alt'])
+                        print("Img Save Success: " + str(currentImageAmount))
+                        currentImageAmount += 1
+                        if currentImageAmount > maxAmount:
+                            break
+                except ValueError:
+                    continue
+        startNum += 50
+    '''
+    
     # 이미지 url https://www.google.com/search?q=검색내용&tbm=isch
     # 구글은 기본적으로 20개를 불러오는 방식을 이용함
     # 이 때 start 인자를 이용하면 시작지점을 정할 수 있어 20개단위로 여러번 작동시켜 원하는만큼 받아오도록 함
+    
     googleUrl = "https://www.google.com/search?"
 
     startNum = 0
     currentImageAmount = 1
 
     # 기본적으로 요청량만큼 받지만 이미지 다운로드에 실패하여 startNum이 너무 늘어날 경우 그냥 끝냄
-    while currentImageAmount <= maxAmount or startNum > maxAmount * 3:
+    while currentImageAmount <= maxAmount and startNum < maxAmount * 3:
         params = {
             "q": name,
             "tbm": "isch",
             "start": startNum
         }
-        htmlData = req.get(googleUrl, params)
+
+        htmlData = requests.get(googleUrl, params)
 
         # 데이터 불러오는데에 성공했을 경우에 사용
         if htmlData.status_code == 200:
             soup = BeautifulSoup(htmlData.text, "html.parser")
-            imgDatas = soup.find_all("img", {'src': True})
+            imgDatas = soup.find_all("img")
+            for img in imgDatas:
+                print (img)
 
             dirName = "data/IMG/" + name + '/'
 
@@ -44,13 +112,14 @@ def Crawling_Image(name, maxAmount):
                     filename = dirName + name + str(currentImageAmount) + '.jpg'
                     with open(filename, 'wb') as f:
                         f.write(img)
-                        print(i[1].attrs['src'])
+                        print(i[1])
                         print("Img Save Success: " + str(currentImageAmount))
                         currentImageAmount += 1
                 except ValueError:
                     continue
 
-            startNum += 20
+        startNum += 20
+    '''
 
 
-Crawling_Image('박효신', 100)
+Crawling_Image('휘인', 30)
