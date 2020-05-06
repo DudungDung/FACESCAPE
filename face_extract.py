0cc5000ab3848719a7d8953bf1915123f7ecd546
Shuu <ghwls328@naver.com>
2020-05-06 22:00:58 +0900

    유사도 변경 및 모델 저장

--- face_extract.py
+++ face_extract.py
@@ -16,7 +16,7 @@ def imread_utf8(img_path, flags):
         return None
 
 
-def imwrite_utf8(img_path, img, params = None):
+def imwrite_utf8(img_path, img, params=None):
     try:
         ext = os.path.splitext(img_path)[1]
         result, n = cv2.imencode(ext, img, params)
@@ -33,6 +33,7 @@ def imwrite_utf8(img_path, img, params = None):
 
 
 def face_extractor(name):  ## 호출시 이름 입력
+    global model
     data_path = 'data/IMG/' + name + '/'
 
     if not os.path.exists(data_path):
@@ -53,6 +54,9 @@ def face_extractor(name):  ## 호출시 이름 입력
     Training_Data.append(np.asarray(images, dtype=np.uint8))
     Labels.append(0)
 
+    train_confidence = 70
+    max_confidence = 80
+
     for i, files in enumerate(onlyfiles):
         image_path = data_path + onlyfiles[i]
         image = imread_utf8(image_path, cv2.IMREAD_COLOR)
@@ -72,18 +76,19 @@ def face_extractor(name):  ## 호출시 이름 입력
             print("Error: Creating directory: " + dirName)
 
         for (x, y, w, h) in faces:
-            print('Getting face ' + str(i + 1))
+            print('Getting face from ' + image_path)
             cropped_face = image[y:y + h, x:x + w]
             img = cv2.resize(cropped_face, (200, 200))
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             result = model.predict(img)
             if result[1] < 500:
                 confidence = int(100 * (1 - (result[1]) / 300))
-                if confidence > 73:  # 유사도가 73퍼 이상이면 넣어줌
-                    print('Get ' + name + ' face')
+                print('Confidence :' + confidence.__str__())
+                if confidence >= train_confidence or (count == 0 and confidence > 60):  # 학습 시작 전에는 유사도 70% 이상일 때 넣어줌
                     img = cv2.resize(cropped_face, (200, 200))
                     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                     count += 1
+                    print('Get ' + name + ' face' + count.__str__())
                     file_name_path = dirName + 'Learning_IMG' + str(count) + '.jpg'
                     imwrite_utf8(file_name_path, img)
                     Training_Data.append(np.asarray(img, dtype=np.uint8))
@@ -91,5 +96,29 @@ def face_extractor(name):  ## 호출시 이름 입력
                     Labelf = np.asarray(Labels, dtype=np.int32)
                     model = cv2.face.LBPHFaceRecognizer_create()
                     model.train(np.asarray(Training_Data), np.asarray(Labelf))
+                    if train_confidence < max_confidence and count >= 100:
+                        train_confidence += 0.1
+                        if train_confidence >= max_confidence:
+                            train_confidence = int(max_confidence)
             else:
                 print("Face not found")
+
+    modelDir = 'data/model/'
+    try:
+        if not os.path.exists(modelDir):
+            os.makedirs(modelDir)
+            print("Create Directory: " + modelDir)
+
+    except OSError:
+        print("Error: Creating directory: " + modelDir)
+
+    try:
+        if os.path.exists(modelDir + name + ".model"):
+            os.remove(modelDir + name + ".model:")
+            print("Remove already existing file: " + name + ".model")
+    except OSError:
+        print("Error: Cant remove file: " + name + ".model")
+
+    model.save(modelDir + "trainer.model")
+    os.rename(modelDir + "trainer.model", modelDir + name + ".model")
+    print("Save Model File: " + modelDir + name + ".model")
