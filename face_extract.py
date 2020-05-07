f95f2256fb7b707b0e3636a2c938ab88f5df058c
Shuu <ghwls328@naver.com>
2020-05-07 21:50:22 +0900

    머신러닝 구동방식 변경 부하낮추기

--- face_extract.py
+++ face_extract.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 from os import listdir
 from os.path import isfile, join
+import time
 
 
 def imread_utf8(img_path, flags):
@@ -48,60 +49,94 @@ def face_extractor(name):  ## 호출시 이름 입력
     if onlyfiles.__len__() <= 0:
         print("사진이 존재하지 않습니다.")
         return
+
     Training_Data, Labels = [], []
-    image_path = data_path + onlyfiles[0]  # 폴더 내의 사진의 1번은 특정이라 생각함
-    images = imread_utf8(image_path, cv2.IMREAD_GRAYSCALE)  # 한번 머신러닝
-    Training_Data.append(np.asarray(images, dtype=np.uint8))
-    Labels.append(0)
+    model = cv2.face.LBPHFaceRecognizer_create()
 
-    train_confidence = 70
-    max_confidence = 80
+    dirName = 'data/Learning/' + name + '/'
+    try:
+        if not os.path.exists(dirName):
+            os.makedirs(dirName)
+            print("Create Directory: " + dirName)
 
-    for i, files in enumerate(onlyfiles):
-        image_path = data_path + onlyfiles[i]
-        image = imread_utf8(image_path, cv2.IMREAD_COLOR)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-        Labelf = np.asarray(Labels, dtype=np.int32)
-        model = cv2.face.LBPHFaceRecognizer_create()
-        model.train(np.asarray(Training_Data), np.asarray(Labelf))
+    except OSError:
+        print("Error: Creating directory: " + dirName)
 
-        dirName = 'data/Learning/' + name + '/'
-        try:
-            if not os.path.exists(dirName):
-                os.makedirs(dirName)
-                print("Create Directory: " + dirName)
+    skipped_Number = []
+    train_confidence = 75
+    max_confidence = 85
+    end_Amount = 0
 
-        except OSError:
-            print("Error: Creating directory: " + dirName)
+    while count < 1000:
+        for i, files in enumerate(onlyfiles):
+            if i in skipped_Number:
+                continue
+            image_path = data_path + onlyfiles[i]
+            image = imread_utf8(image_path, cv2.IMREAD_COLOR)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces = face_classifier.detectMultiScale(gray, 1.3, 5)
 
-        for (x, y, w, h) in faces:
-            print('Getting face from ' + image_path)
-            cropped_face = image[y:y + h, x:x + w]
-            img = cv2.resize(cropped_face, (200, 200))
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            result = model.predict(img)
-            if result[1] < 500:
-                confidence = int(100 * (1 - (result[1]) / 300))
-                print('Confidence :' + confidence.__str__())
-                if confidence >= train_confidence or (count == 0 and confidence > 60):  # 학습 시작 전에는 유사도 70% 이상일 때 넣어줌
+            dirName = 'data/Learning/' + name + '/'
+            try:
+                if not os.path.exists(dirName):
+                    os.makedirs(dirName)
+                    print("Create Directory: " + dirName)
+
+            except OSError:
+                print("Error: Creating directory: " + dirName)
+
+            for (x, y, w, h) in faces:
+                print('Getting face from ' + image_path)
+                cropped_face = image[y:y + h, x:x + w]
+                img = cv2.resize(cropped_face, (200, 200))
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                if count == 0:
                     img = cv2.resize(cropped_face, (200, 200))
                     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                     count += 1
+                    if count % 200 == 0:  # 200개단위로 증가했을 때 확률을 좁혀줌
+                        max_confidence += count // 200
+                    skipped_Number.insert(i, i)
                     print('Get ' + name + ' face' + count.__str__())
-                    file_name_path = dirName + 'Learning_IMG' + str(count) + '.jpg'
+                    file_name_path = dirName + 'Learning_IMG' + f'{count:04}' + '.jpg'
                     imwrite_utf8(file_name_path, img)
                     Training_Data.append(np.asarray(img, dtype=np.uint8))
                     Labels.append(count)
                     Labelf = np.asarray(Labels, dtype=np.int32)
-                    model = cv2.face.LBPHFaceRecognizer_create()
                     model.train(np.asarray(Training_Data), np.asarray(Labelf))
-                    if train_confidence < max_confidence and count >= 100:
-                        train_confidence += 0.1
-                        if train_confidence >= max_confidence:
-                            train_confidence = int(max_confidence)
-            else:
-                print("Face not found")
+                    continue
+                result = model.predict(img)
+                if result[1] < 500:
+                    confidence = int(100 * (1 - (result[1]) / 300))
+                    print('Confidence :' + confidence.__str__())
+                    if confidence >= train_confidence:  # 학습 시작 전에는 무조건 입력, 그 후 확률을 만족했을 경우만 넣어줌
+                        img = cv2.resize(cropped_face, (200, 200))
+                        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                        count += 1
+                        if count % 200 == 0: # 200개단위로 증가했을 때 확률을 좁혀줌
+                            max_confidence += count//200
+                        skipped_Number.insert(i, i)
+                        print('Get ' + name + ' face' + count.__str__())
+                        file_name_path = dirName + 'Learning_IMG' + f'{count:04}' + '.jpg'
+                        imwrite_utf8(file_name_path, img)
+                        Training_Data.append(np.asarray(img, dtype=np.uint8))
+                        Labels.append(count)
+                        Labelf = np.asarray(Labels, dtype=np.int32)
+                        model.train(np.asarray(Training_Data), np.asarray(Labelf))
+
+                        if train_confidence < max_confidence:
+                            train_confidence += 0.2
+                            if train_confidence >= max_confidence:
+                                train_confidence = int(max_confidence)
+
+                else:
+                    print("Face not found")
+
+        if end_Amount == count:
+            print("Lower confidence condition")
+            train_confidence -= 1
+            max_confidence -= 1
+        end_Amount = count
 
     modelDir = 'data/model/'
     try:
@@ -122,3 +157,4 @@ def face_extractor(name):  ## 호출시 이름 입력
     model.save(modelDir + "trainer.model")
     os.rename(modelDir + "trainer.model", modelDir + name + ".model")
     print("Save Model File: " + modelDir + name + ".model")
+    print("Learning " + str(count) + "Pictures")
