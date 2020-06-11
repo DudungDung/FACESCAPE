167722941771c20d8b6e15c65f7e6395445291ff
Shuu <ghwls328@naver.com>
2020-06-11 12:44:17 +0900

    스플릿 이용, 여러명 인물 탐색

--- WebCrawler.py
+++ WebCrawler.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 import os
 
 import re
-import face_clustering as fc
 import face_detect as fd
 
 notAllowedChar = '^[/:*?"<>|\\\\ ]+$'
@@ -26,13 +25,14 @@ def Allow_Certain_Folder_Name(string):
     return True
 
 
-def Crawling_Image(name, maxAmount):
-    if not Allow_Certain_Folder_Name(name):
+def Crawling_Image(search, maxAmount):
+    if not Allow_Certain_Folder_Name(search):
         return
 
-    for c in '/:*?"<>|\\':
-        name = name.replace(c, "")
+    for c in '/:*?"<>|\\ ':
+        search = search.replace(c, "")
 
+    names = search.split(',')
     # 이미지 url https://search.naver.com/search.naver?where=image&query=검색이름
     # 네이버는 기본적으로 50개를 불러오는 방식을 이용함
     # 이 때 start 인자를 이용하면 시작지점을 정할 수 있어 50개단위로 여러번 작동시켜 원하는만큼 받아오도록 함
@@ -40,57 +40,56 @@ def Crawling_Image(name, maxAmount):
     # 원래 구글을 이용하려 하였으나 구글을 통해 받은 이미지 해상도가 너무 좋지 않아 네이버로 변경
     # 또한 네이버는 이미지 소스에 data-source가 있는 경우를 통해 제한할 경우 구글처럼 로고가 포함되는 경우가 없어 오류 조정에도 더 편함
     # 한국 인물 특성상 네이버를 통해서도 많은 이미지를 얻을 수 있기 때문에 네이버로 이용
+    for name in names:
+        naverUrl = "https://search.naver.com/search.naver?"
 
-    naverUrl = "https://search.naver.com/search.naver?"
+        startNum = 0
+        currentImageAmount = 1
 
-    startNum = 0
-    currentImageAmount = 1
+        dirName = f"data/IMG/{name}/"
+        try:
+            if not os.path.exists(dirName):
+                os.makedirs(dirName)
+                print("Create Directory: " + dirName)
 
-    dirName = f"data/IMG/{name}/"
-    try:
-        if not os.path.exists(dirName):
-            os.makedirs(dirName)
-            print("Create Directory: " + dirName)
+        except OSError:
+            print("Error: Creating directory: " + dirName)
 
-    except OSError:
-        print("Error: Creating directory: " + dirName)
+        # 기본적으로 요청량만큼 받지만 이미지 다운로드에 실패하여 startNum이 너무 늘어날 경우 그냥 끝냄
+        while currentImageAmount <= maxAmount and startNum < maxAmount * 2:
+            params = {
+                "query": name,
+                "where": "image",
+                "start": startNum
+            }
 
-    # 기본적으로 요청량만큼 받지만 이미지 다운로드에 실패하여 startNum이 너무 늘어날 경우 그냥 끝냄
-    while currentImageAmount <= maxAmount and startNum < maxAmount * 2:
-        params = {
-            "query": name,
-            "where": "image",
-            "start": startNum
-        }
+            htmlData = requests.get(naverUrl, params)
+            if htmlData.status_code == 200:
+                soup = BeautifulSoup(htmlData.text, 'html.parser')
+                imgs = soup.find_all('img', {'data-source': True})
+                # 검색을 해서 만약에 검색결과가 안나오는 경우 실행을 안함
+                if imgs is not None:
+                    for i in enumerate(imgs):
+                        try:
+                            img = urlopen(i[1].attrs['data-source']).read()
+                            filename = dirName + 'IMG' + f'{currentImageAmount:05}' + '.jpg'
+                            tempFileName = dirName + 'Temp' + f'{currentImageAmount:05}' + '.jpg'
+                            with open(tempFileName, "wb") as f:
+                                f.write(img)
+                                f.close()
 
-        htmlData = requests.get(naverUrl, params)
-        if htmlData.status_code == 200:
-            soup = BeautifulSoup(htmlData.text, 'html.parser')
-            imgs = soup.find_all('img', {'data-source': True})
-            # 검색을 해서 만약에 검색결과가 안나오는 경우 실행을 안함            
-            if imgs is not None:
-                for i in enumerate(imgs):
-                    try:
-                        img = urlopen(i[1].attrs['data-source']).read()
-                        filename = dirName + 'IMG' + f'{currentImageAmount:05}' + '.jpg'
-                        tempFileName = dirName + 'Temp' + f'{currentImageAmount:05}' + '.jpg'
-                        with open(tempFileName, "wb") as f:
-                            f.write(img)
-                            f.close()
+                            if fd.find_one_face_dnn(tempFileName, filename):
+                                print(i[1].attrs['alt'])
+                                print("Img Save Success: " + str(currentImageAmount))
+                                currentImageAmount += 1
 
-                        if fd.find_one_face_dnn(tempFileName, filename):
-                            print(i[1].attrs['alt'])
-                            print("Img Save Success: " + str(currentImageAmount))
-                            currentImageAmount += 1
+                            if currentImageAmount > maxAmount:
+                                break
+                        except ValueError:
+                            continue
 
-                        if currentImageAmount > maxAmount:
-                            break
-                    except ValueError:
-                        continue
+            startNum += 50
 
-        startNum += 50
-
-    #fc.sk_clustering(dirName)
     # 이미지 url https://www.google.com/search?q=검색내용&tbm=isch
     # 구글은 기본적으로 20개를 불러오는 방식을 이용함
     # 이 때 start 인자를 이용하면 시작지점을 정할 수 있어 20개단위로 여러번 작동시켜 원하는만큼 받아오도록 함
